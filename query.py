126656ced29b7891ee5fbf9e9c7e32f4abb0d234
Sahil Samar <sahilsamar031@gmail.com>
2024-06-01 02:31:05 -0700

    Added argument to change llm model

--- query.py
+++ query.py
@@ -7,41 +7,42 @@ from langchain.prompts import ChatPromptTemplate
 CHROMA_PATH = "chroma"
 
 PROMPT_TEMPLATE = """
-Answer the question based only on the following context:
+Answer the question based only on the following sources:
 
 {context}
 
 ---
 
-Answer the question based on the above context: {question}
-
-The format of the answer should be a numbered list of relevant materials or events.
+Answer the question by providing a numbered list of sources with URLs and descriptions ONLY coming from the above list: {question}
 """
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("query_text", type=str, help="The query text.")
+    parser.add_argument("model", type=str, help="model")
     args = parser.parse_args()
     query_text = args.query_text
+    model = args.model
 
     embedding_function = ollama.OllamaEmbeddings(model='nomic-embed-text')
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
-    results = db.similarity_search(query_text, k=3)
+    results = db.similarity_search(query_text, k=20)
     if len(results) == 0:
-        print(f"Unable to find matching results.")
+        print("Unable to find matching results.")
         return
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
 
-    model = ChatOllama(model="llama3")
+    model = ChatOllama(model=model)
     response_text = model.predict(prompt)
 
     sources = [doc.metadata.get("Title", None) for doc in results]
-    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    # formatted_response = f"Response: {response_text}\n Sources:{sources}"
+    formatted_response = f"Response: {response_text}"
     print(formatted_response)
 
 
